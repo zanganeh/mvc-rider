43005e54eb1ed100cda32a8ab7861cde57970302
zanganeh@gmail.com <zanganeh@gmail.com@24cec82a-7371-6400-f6d5-38cde0e735da>
2011-03-13 11:54:52 +0000

    Adding web application

---
+++ MVCRider/Controllers/AdminController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCRiderModel;
+
+namespace MVCRider.Controllers
+{
+    public class AdminController : Controller
+    {
+        MVCRiderModel.MVCRiderDataBaseEntities _dx = new MVCRiderModel.MVCRiderDataBaseEntities();
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Posts()
+        {
+            return View(_dx.Posts.ToList());
+        }
+
+        public ActionResult PostEdit(int id)
+        {
+            var currentPost = _dx.Posts.Where(a => a.ID == id).FirstOrDefault();
+            return View(currentPost);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]        
+        public ActionResult PostEdit(int id, FormCollection collection)
+        {
+            var currnetPost = _dx.Posts.FirstOrDefault(a => a.ID == id);
+            if (TryUpdateModel(currnetPost))
+            {
+                _dx.SaveChanges();
+                return RedirectToAction("Posts");
+            }
+            else
+            {
+                return View(currnetPost);
+            }
+        }
+
+        public ActionResult PostCreate()
+        {
+            return View();
+        }
+
+        [AcceptVerbs( HttpVerbs.Post)]
+        public ActionResult PostCreate(Post currentPost)
+        {
+            _dx.Posts.AddObject(currentPost);
+            _dx.SaveChanges();
+            return RedirectToAction("Posts");
+        }
+    }
+}
---
+++ MVCRider/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCRider.Controllers
+{
+    public class HomeController : Controller
+    {
+        MVCRiderModel.MVCRiderDataBaseEntities _dx = new MVCRiderModel.MVCRiderDataBaseEntities();
+
+        public ActionResult Index()
+        {
+            var posts = _dx.Posts
+                .Where(a =>
+                    (a.Active ?? false) == true && (!a.ReleaseDate.HasValue || (a.ReleaseDate.HasValue && a.ReleaseDate.Value > DateTime.Now)))
+                .OrderByDescending(a => a.CreatedDate)
+                .ToList();
+            return View(posts);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
