a1d2902908906140088562b1738f28d517729c51
zanganeh@gmail.com <zanganeh@gmail.com@24cec82a-7371-6400-f6d5-38cde0e735da>
2011-09-11 01:36:17 +0000

    Advanced topic: Dispatch/client runtime extensibility

---
+++ Demo/WCF/DispatchClientRuntimeExtensibility/Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+            ServerManager.CalculatorClient client = new ServerManager.CalculatorClient("NetTcpBinding_ICalculator");
+            Console.WriteLine("This is a client");
+            var result = client.Add(a, b);
+            Console.WriteLine("'Add' function has been called with current parameter : a='{0}',b='{1}' and the result is c='{2}'", a, b, result);
+        }
+    }
+}
